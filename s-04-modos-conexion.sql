bb6e356cdd3c4c4539009b189a619594b8b7ab3e
johanzarazua <johanzarazua@gmail.com>
2021-12-30 22:15:06 -0600

    Modos de conexion

---
+++ s-04-modos-conexion.sql
@@ -0,0 +1,38 @@
+--@Autor Hernández Gómez Alejandro, Zarazúa Ramírez Johan
+--@Fecha 27/12/2021
+--@Descripcion Configuración del modo compartido
+
+whenever sqlerror exit rollback;
+
+--Autenticación con el usuario sys 
+connect sys/system as sysdba
+
+alter system set dispatchers='(dispatchers=2)(protocol=tcp)';
+alter system set shared_servers=4;
+
+alter system register;
+
+--Configuración tnsnames.ora
+/*
+HEZAPROY =
+  (DESCRIPTION =
+    (ADDRESS_LIST =
+      (ADDRESS = (PROTOCOL = TCP)(HOST = pc-ahg.fi.unam)(PORT = 1521))
+    )
+    (CONNECT_DATA =
+      (SERVICE_NAME = hezaproy)
+      (SERVER = DEDICATED)
+    )
+  )
+
+HEZAPROY_SHARED =
+  (DESCRIPTION =
+    (ADDRESS_LIST =
+      (ADDRESS = (PROTOCOL = TCP)(HOST = pc-ahg.fi.unam)(PORT = 1521))
+    )
+    (CONNECT_DATA =
+      (SERVICE_NAME = hezaproy)
+      (SERVER = SHARED)
+    )
+  )
+*/
