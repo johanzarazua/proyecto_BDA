2b27fb012daa350f5ccddcdbe622b4fd7a99e045
johanzarazua <johanzarazua@gmail.com>
2022-01-01 14:48:37 -0600

    Carga inicial

---
+++ s-13-carga-datos-biblioteca.sql
@@ -0,0 +1,54 @@
+--@Autor:           Hernández Gómez Alejandro, Zarazúa Ramírez Johan
+--@Fecha creación:  29/12/2021
+--@Descripción:     Carga de datos biblioteca
+
+--si ocurre un error, se hace rollback de los datos y
+--se sale de SQL *Plus
+whenever  sqlerror exit rollback
+
+Prompt conectando usuario heza_biblioteca
+connect heza_biblioteca/hezab@HEZAPROY
+
+set define off
+
+Prompt realizando la carga de datos al modulo biblioteca
+@@datos_iniciales/biblioteca.sql
+@@datos_iniciales/area_conocimiento.sql
+@@datos_iniciales/status_recurso.sql
+@@datos_iniciales/recurso.sql
+@@datos_iniciales/historico_status_recurso.sql
+@@datos_iniciales/palabra_clave.sql
+@@datos_iniciales/autor.sql
+@@datos_iniciales/editorial.sql
+@@datos_iniciales/libro.sql
+@@datos_iniciales/revista.sql
+@@datos_iniciales/tesis.sql
+@@datos_iniciales/lista_area_c.sql
+@@datos_iniciales/autor_libro.sql
+
+
+
+set define on
+
+update recurso set tipo = 'L' where recurso_id in (
+  select recurso_id from libro
+); 
+
+update recurso set tipo = 'R' where recurso_id in (
+  select recurso_id from revista
+); 
+
+update recurso set tipo = 'T' where recurso_id in (
+  select recurso_id from tesis
+);
+
+Prompt confirmando cambios
+commit;
+
+--Si se encuentra un error, no se sale de SQL *Plus
+--no se hace commit ni rollback, es decir, se
+--regresa al estado original.
+whenever sqlerror continue none
+
+Prompt Listo!
+disconnect
