2a5b050d636f57363749303819e05034589d257a
johanzarazua <johanzarazua@gmail.com>
2022-01-03 17:50:47 -0600

    FRA

--- s-24-copia-control-file-FRA.sql
+++ s-24-copia-control-file-FRA.sql
@@ -7,19 +7,18 @@ conn sys/system as sysdba
 -- Consultamos el valor de los control Files
 show parameter control_files;
 
--- Se detiene la instancia y se inicia en modo nomount
-shutdown immediate
-startup nomount
-
 --Editar el parametro control file agregando el nombre del archivo de control creado en la FRA
 alter system set 
   control_files = '/u01/app/oracle/oradata/HEZAPROY/disk_1/control01.ctl', '/u01/app/oracle/oradata/HEZAPROY/disk_2/control02.ctl', '/u01/app/oracle/oradata/HEZAPROY/disk_fra/control03.ctl'
   scope = spfile;
-
 -- Configuracion original
 -- alter system set 
 --   control_files = '/u01/app/oracle/oradata/HEZAPROY/disk_1/control01.ctl','/u01/app/oracle/oradata/HEZAPROY/disk_2/control02.ctl','/u01/app/oracle/oradata/HEZAPROY/disk_3/control03.ctl'
 --   scope = spfile;
+
+-- Se detiene la instancia y se inicia en modo nomount
+shutdown immediate
+startup nomount
 
 -- Utilizando rman para mover el control file
 -- !echo "restore controlfile to '/u01/app/oracle/oradata/HEZAPROY/disk_fra/control03.ctl' from '/u01/app/oracle/oradata/HEZAPROY/disk_3/control03.ctl'; " | rman target /
@@ -31,3 +30,11 @@ alter system set
 --detener la instancia y levantarla en modo normal 
 shutdown immediate
 startup
+
+set linesisze window
+col name for a60
+select status, name, is_recovery_dest_file from v$controlfile;
+
+select * from v$recovery_file_dest;
+
+select * from v$recovery_file_dest;
